b880f2c09b7673d212fb940bc79b1b896751c0a7
metavin007 <meta.vin@hotmail.com>
2015-11-18 15:13:20 +0700

    add pesent

--- src/main/webapp/js/calendarpatient.js
+++ src/main/webapp/js/calendarpatient.js
@@ -7,7 +7,10 @@ angular.module('calendarPatient')
             $scope.start = '';
             $scope.end = '';
 
-            $scope.dateClick = '';
+            $scope.searchData = {};
+            $scope.appointments = {};
+
+            $scope.showAppointmentsDay = '';
 
             setAppointment();
             function setAppointment() {
@@ -15,9 +18,12 @@ angular.module('calendarPatient')
                 $http.get('/getappointment').success(function (data) {
                     for (var i = 0; i < data.content.length; i++) {
                         events.push({title: data.content[i].patient.name
-//                                    +" \nนัดกับหมอ \n "+
-//                                    +" \n "+data.content[i].doctor.nameTh 
-                                    + " \n " + data.content[i].patient.mobile,
+                                    + "\n เบอร์โทร "+
+                                    data.content[i].patient.mobile,
+//                                    + " \n นัดกับหมอ \n" +
+//                                    data.content[i].doctor.nameTh
+//                                    + 
+//                                    data.content[i].doctor.mobile,
                             start: data.content[i].appointDay + 'T' + data.content[i].startTime,
                             end: data.content[i].appointDay + 'T' + data.content[i].endTime
                         });
@@ -59,15 +65,20 @@ angular.module('calendarPatient')
                     eventClick: function (calEvent) {
                         $scope.title = calEvent.title;
                         $scope.start = moment(new Date(new Date(calEvent.start).getFullYear(), new Date(calEvent.start).getMonth(), new Date(calEvent.start).getDate()
-                                , new Date(calEvent.start).getHours() - 7)).format('YYYY-MM-D เวลา hh:mm a');
+                                , new Date(calEvent.start).getHours() - 7)).format('YYYY-MM-D เวลา HH:mm');
                         $scope.end = moment(new Date(new Date(calEvent.end).getFullYear(), new Date(calEvent.end).getMonth(), new Date(calEvent.end).getDate()
-                                , new Date(calEvent.end).getHours() - 7)).format('YYYY-MM-D เวลา hh:mm a');
+                                , new Date(calEvent.end).getHours() - 7)).format('YYYY-MM-D เวลา HH:mm');
                         setAppointment();
                         $('#modal-showDataEvent').openModal();
                     },
                     dayClick: function (date) {
-                        $scope.dateClick = date.format();
-                        setAppointment();
+                        $scope.showAppointmentsDay = date.format();
+                        $scope.searchData.keyword = date.format();
+                        $scope.searchData.searchBy = 'วันที่นัด';
+                        $http.post('/searchappointment', $scope.searchData).success(function (data) {
+                            $scope.appointments = data;
+                        }).error(function (data) {
+                        });
                         $('#modal-showDataDay').openModal();
                     }
                 });
